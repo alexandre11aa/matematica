aab3ce5f6a02258d2fe7c2c213b8ecb24c97f30b
Alexandre Estrela <82901702+alexandre11aa@users.noreply.github.com>
2021-06-18 13:28:44 -0300

    Update sistemas_lineares-metodo_de_gauss.py

--- sistemas_lineares-metodo_de_gauss.py
+++ sistemas_lineares-metodo_de_gauss.py
@@ -1,89 +1,63 @@
 def sinal(nmr):
     if nmr >= 0:
-        nmr = "+%.5f" % nmr
+        nmr = "+%3.5f" % nmr
     else:
-        nmr = "%.5f" % nmr
+        nmr = "%3.5f" % nmr
 
     return nmr
 
-print("O programa só irá funcionar se o número de colunas"
-      "for uma unidade maior que o número de linhas, além"
-      "da matriz passar pelas condições de convergência !")
+l = int(input("Informe o número de linhas: "))
 print("")
 
-'''
+m_i, m_x, m_d, z = [[],[],[],0]
 
-- Exemplo de input:
-
-3
-10
-0.01
-
-20 7 9 16
-7 30 8 38
-9 8 30 38
-
-'''
-
-mi_t, m1_a = [[],[]]
-
-l = int(input("Número de linhas: "))
-it = int(input("Número máximo de iterações: "))
-ve = float(input("Porcentagem de erro: "))
+for i in range(l):
+    m_i.append(list(map(float,input("Insira a linha %.i da matriz: " % i).split())))
 print("")
 
+if m_i[0][0] == 0:
+    for i in range(l-1,-1,-1):
+        print(i)
+        if m_i[i][0] != 0:
+            z = i
+
+    m_i[0], m_i[z] = m_i[z], m_i[0]
+
+c1, c2 = [1,0]
+
+for k in range(l-1):
+    for i in range(c1,l):
+        n_m = (m_i[i][k] * -1 / m_i[c2][k])
+        for j in range(l+1):
+            m_i[i][j] += n_m * m_i[c2][j]
+
+    c1 += 1
+    c2 += 1
+
 for i in range(l):
-    mi_t.append(list(map(float,input("Informe a linha %.i: " % (i + 1)).split())))
+    m_x.append(m_i[i][l])
+    for j in range(l):
+        if i == j:
+            m_d.append(m_i[i][j])
+
+c1 = l
+c2 = 0
+
+for i in range(l-1,-1,-1):
+    for j in range(c2):
+        m_x[i] -= m_i[i][l - 1 - j] * m_x[l - 1 - j]
+
+    m_x[i] /= m_d[i]
+
+    c1 -= 1
+    c2 += 1
+
+print(" |       x0 |", end="")
+for i in range(1,l):
+    print("       x%.f |" % i, end="")
+
 print("")
-print(" |    I |", end="")
 
-for i in range(l):
-    print("       x%.i |" % (i + 1), end="")
-
-print("        e |")
-
-for i in range(l):
-    m1_a.append(mi_t[i][l]/mi_t[i][i])
-
-c = 0
-
-while True:
-    m1_p, m1_f = [[],[]]
-
-    for i in range(l):
-        nm_x = mi_t[i][l]/mi_t[i][i]
-        for j in range(l):
-            if j != i:
-                nm_x += mi_t[i][j] * m1_a[j] * -1 / mi_t[i][i]
-
-        m1_p.append(nm_x)
-
-    for i in range(l):
-        m1_f.append(m1_p[i] - m1_a[i])
-
-        if m1_f[i] < 0:
-            m1_f[i] *= -1
-
-    x_m1, x_m2 = [0,0]
-
-    for i in range(l):
-        if m1_p[i] > x_m1:
-            x_m1 = m1_p[i]
-
-        if m1_f[i] > x_m2:
-            x_m2 = m1_f[i]
-
-    for i in range(l):
-        m1_a[i] = m1_p[i]
-
-    c += 1
-
-    print(" | " + "%4i" % c, end="")
-
-    for i in range(l):
-        print(" | " + sinal(m1_p[i]), end="")
-
-    print(" | " + sinal(x_m2 / x_m1) + " | ")
-
-    if (x_m2 / x_m1 < ve) or it == c:
-        break
+print(" | " + sinal(m_x[0]) + " |", end="")
+for i in range(1,l):
+    print(" " + sinal(m_x[i]) + " |", end="")
